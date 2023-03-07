dd59038c97d9f69345d53a6de25eb11f9e0cafb5
sanremy <skyst123@gmail.com>
2023-03-07 17:58:46 -0300

    Atualização

--- Fibonacci_teste.py
+++ Fibonacci_teste.py
@@ -3,22 +3,22 @@ soma = 0
 k = 0
 
 while k < indice:
-    k = k + 1
+    k += 1
     soma = soma + k
 print(soma)
 
 num = int(input("Digite um número inteiro: "))
 
-fib1 = 0
-fib2 = 1
-fib3 = 1
+fibo1 = 0
+fibo2 = 1
+fib03 = 1
 
-while fib3 < num:
-    fib1 = fib2
-    fib2 = fib3
-    fib3 = fib1 + fib2
+while fibo3 < num:
+    fibo1 = fibo2
+    fibo2 = fibo3
+    fibo3 = fibo1 + fibo2
 
-if fib3 == num:
-    print(num, "pertence à sequência de Fibonacci")
+if fibo3 == num:
+    print("O número " + str(num) + "pertence à sequência de Fibonacci")
 else:
-    print(num, "não pertence à sequência de Fibonacci")
+    print("O número " + str(num) + "não pertence à sequência de Fibonacci")
